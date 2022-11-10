56ae6b4c3dccc0d617d9e70b6b68e392094161ee
a-s-wijaya <adrian.s.wijaya@upi.edu>
2022-11-10 22:35:07 +0700

    Section A Fix & Create Section B
    Can proceed to finishing for both section after this

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,19 @@
 window.onload = function() {
     feather.replace();
+    var splide = new Splide('.splide', {
+        type: 'loop',
+        perPage: 3,
+        rewind: true,
+        breakpoints: {
+            768: {
+                perPage: 2,
+                gap: '.7rem',
+            },
+            576: {
+                perPage: 1,
+                gap: '.7rem',
+            },
+        },
+    });
+    splide.mount();
 }
