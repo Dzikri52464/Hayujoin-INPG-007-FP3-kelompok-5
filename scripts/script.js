51cce6f3f9e558b39f0a7526f95895bb2bcd2b21
Dzikri52464 <111979924+Dzikri52464@users.noreply.github.com>
2022-11-14 18:54:09 +0700

    scroll

--- scripts/script.js
+++ scripts/script.js
@@ -238,3 +238,18 @@ contactForm.addEventListener('submit', function(e) {
         contactForm.reset();
     }
 });
+
+window.addEventListener("scroll", muncul);
+function muncul(){
+    let items = document.querySelectorAll(".item");
+    for (let i = 0; i < items.length; i++){
+        let Tlayar = window.innerHeight;
+        let Jitem = items[i].getBoundingClientRect().top;
+        let Uscrol = 150;
+        if ( Jitem < Tlayar - Uscrol){
+            items[i].classList.add("tampil");
+        } else{
+            items[i].classList.remove("tampil")
+        }
+    }
+}
